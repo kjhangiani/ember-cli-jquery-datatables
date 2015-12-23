5189dd9fa2b1d39a702ec006d9d12ad5b8592a4f
Bastien Cecchinato <bcecchinato@users.noreply.github.com>
2015-12-23 14:14:59 +0100

    Adding an option parameter

--- addon/components/jquery-datatable.js
+++ addon/components/jquery-datatable.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'table',
   classNames: ['table','table-striped','table-bordered','dataTable'],
+  options: {},
+  
   initDataTable: function() {
 
-    this.$().dataTable({});
+    this.$().dataTable(this.get('options'));
 		
   }.on('didInsertElement')
 });
